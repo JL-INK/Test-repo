43aa1868206b6a91bd0b78dbf1621301a0e47b20
JL-INK <valera.frantsev@gmail.com>
2020-02-23 21:52:40 +0300

    Создание SqlParemater

--- MyFirstProject/Program.cs
+++ MyFirstProject/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
 using System.Configuration;
+using System.Data;
 
 namespace ConsoleApp1
 {
@@ -13,16 +14,20 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             {
+                //Подключение к БД через App.config
                string connectionString = ConfigurationManager.ConnectionStrings["Shop"].ConnectionString;
-               System.Data.SqlClient.SqlConnection conn = new System.Data.SqlClient.SqlConnection(connectionString);
-                conn.Open();
+               System.Data.SqlClient.SqlConnection connection = new System.Data.SqlClient.SqlConnection(connectionString);
+                connection.Open();
 
+                //Начало цикла возвращения
                 while (true)
                 {
-                    Console.Write ("                              Введите дату поставки");
-                    Console.Write("\n");                    
+                    Console.Write("                              Введите дату поставки");
+                    Console.Write("\n");
                     string text = Console.ReadLine();
                     DateTime saleDate = DateTime.Now;
+
+                    //Проверка вводимых данных
                     try
                     {
                         saleDate = DateTime.Parse(text);
@@ -32,24 +37,33 @@ namespace ConsoleApp1
                         Console.WriteLine("Некорретная дата");
                         continue;
                     }
+                    Console.WriteLine("Продано на " + saleDate);
 
-                    Console.WriteLine("Продано на 2020-01-15");
-                    String sql = @"SELECT s.Id, p.Name, s.Quantity, s.Date From Sales s
-                                   join Products p on P.ID = s.ProductId
-                                   where s.Date = '" + saleDate +"'";
-                    SqlCommand command = new SqlCommand(sql, conn);
-                    SqlDataReader reader = command.ExecuteReader();
-                    while (reader.Read())
-                    {
+                    //Создание Sqlparameter
+                    SqlParameter filter = new SqlParameter("@filter", SqlDbType.VarChar);
+                                 filter.Value = saleDate;
+
+                    //Запрос с применением параметра
+                    SqlCommand request = new SqlCommand(
+                     @"SELECT s.Id, p.Name, s.Quantity, s.Date From Sales s
+                                    join Products p on P.ID = s.ProductId
+                                    where s.Date = @filter", connection);
+                    request.Parameters.Add(filter);
+
+                    //Считывание данных с таблицы
+                     SqlDataReader reader = request.ExecuteReader();
+                     while (reader.Read())
+                      {
                         Console.WriteLine("{0,18} | {1,11} | {2,5} | {3,5}", reader["Id"].ToString(), reader["Name"].ToString(), reader["Quantity"].ToString(), reader["Date"].ToString());
-                    }
-                    reader.Close();
+                      }
+                     reader.Close();
 
+                   //Цикл возврата
                     Console.WriteLine("         Чтобы закрыть нажмите 0 или любую кнопку чтобы начать заного");
                     ConsoleKeyInfo c = Console.ReadKey();
                     if (c.KeyChar == '0') break;
                 }
-                    conn.Close();
+                connection.Close();
             }
 
         }
