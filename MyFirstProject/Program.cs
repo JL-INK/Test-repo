619acd55d81cdf8f96ab79d74b1d1503b51c6d8f
JL-INK <Valera.frantsev@gmail.com>
2020-02-25 16:42:28 +0300

    Фильтрация по запросу

--- MyFirstProject/Program.cs
+++ MyFirstProject/Program.cs
@@ -18,50 +18,80 @@ namespace ConsoleApp1
                string connectionString = ConfigurationManager.ConnectionStrings["Shop"].ConnectionString;
                System.Data.SqlClient.SqlConnection connection = new System.Data.SqlClient.SqlConnection(connectionString);
                 connection.Open();
-
-                //Начало цикла возвращения
-                while (true)
                 {
-                    Console.Write("                              Введите дату поставки");
-                    Console.Write("\n");
-                    string text = Console.ReadLine();
-                    DateTime saleDate = DateTime.Now;
-
-                    //Проверка вводимых данных
-                    try
+                    while (true)
                     {
-                        saleDate = DateTime.Parse(text);
+                        string ent = Console.ReadLine();
+                        String[] request = ent.Split();
+                        var cmd = request[0];
+                        var filter = request[1];
+                        var nOrD = request[2];
+                        switch (cmd)
+                        {
+                            case "get":
+
+                                switch (filter)
+                                {
+                                    case "name":
+
+                                        SqlParameter name = new SqlParameter("@name", nOrD);
+                                        
+                                        SqlCommand product = new SqlCommand(
+                                        @"SELECT * From Products p
+                                             where p.Name = @name", connection);
+                                        product.Parameters.Add(name);
+
+                                        SqlDataReader readerN = product.ExecuteReader();
+                                        while (readerN.Read())
+                                        {
+                                            Console.WriteLine("{0,18} | {1,11} | {2,5}", readerN["Id"].ToString(), readerN["Name"].ToString(), readerN["Description"].ToString());
+                                        }
+                                        readerN.Close();
+                                        
+                                        break;
+
+                                    case "date":
+
+                                        DateTime saleDate = DateTime.Now;
+                                        try
+                                        {
+                                            saleDate = DateTime.Parse(nOrD);
+                                        }
+                                        catch (Exception)
+                                        {
+                                            Console.WriteLine("Некорретная дата");
+                                            continue;
+                                        }
+
+                                        Console.WriteLine("Продано на " + saleDate);
+                                        SqlParameter date = new SqlParameter("@date", saleDate);
+                                        
+                                        SqlCommand balance = new SqlCommand(
+                                         @"select p.Id,p.Name, (u.Quantity-a.Quantity) as Quantity  from Products p
+                                              left join Sales a on a.ProductId = p.Id
+                                              left join Supply u on u.ProductId = p.Id
+                                              where u.Date <= @date and a.Date <= @date", connection);
+                                        balance.Parameters.Add(date);
+
+                                        SqlDataReader readerD = balance.ExecuteReader();
+                                        while (readerD.Read())
+                                        {
+                                            Console.WriteLine("{0,18} | {1,11} | {2,5}", readerD["Id"].ToString(), readerD["Name"].ToString(), readerD["Quantity"].ToString());
+                                        }
+                                        readerD.Close();
+                                        
+                                        break;
+                                }
+
+                                break;
+                            case "delete":
+
+
+
+                                break;
+                        }
+
                     }
-                    catch (Exception)
-                    {
-                        Console.WriteLine("Некорретная дата");
-                        continue;
-                    }
-                    Console.WriteLine("Продано на " + saleDate);
-
-                    //Создание Sqlparameter
-                    SqlParameter filter = new SqlParameter("@filter", SqlDbType.VarChar);
-                                 filter.Value = saleDate;
-
-                    //Запрос с применением параметра
-                    SqlCommand request = new SqlCommand(
-                     @"SELECT s.Id, p.Name, s.Quantity, s.Date From Sales s
-                                    join Products p on P.ID = s.ProductId
-                                    where s.Date = @filter", connection);
-                    request.Parameters.Add(filter);
-
-                    //Считывание данных с таблицы
-                     SqlDataReader reader = request.ExecuteReader();
-                     while (reader.Read())
-                      {
-                        Console.WriteLine("{0,18} | {1,11} | {2,5} | {3,5}", reader["Id"].ToString(), reader["Name"].ToString(), reader["Quantity"].ToString(), reader["Date"].ToString());
-                      }
-                     reader.Close();
-
-                   //Цикл возврата
-                    Console.WriteLine("         Чтобы закрыть нажмите 0 или любую кнопку чтобы начать заного");
-                    ConsoleKeyInfo c = Console.ReadKey();
-                    if (c.KeyChar == '0') break;
                 }
                 connection.Close();
             }
