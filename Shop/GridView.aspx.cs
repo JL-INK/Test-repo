3a068d103759e874ba8ba852d65e38f7d4454e0f
JL-INK <valera.frantsev@gmail.com>
2020-03-03 00:16:32 +0300

    Создана форма

---
+++ Shop/GridView.aspx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services.Description;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Warehouse.Service;
+
+namespace Shop
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void TextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            /*var product = new Product();
+            product.Name = name;
+            product.Description = description;
+            service.AddProduct(product);*/
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            /*service.DeleteFromTable(name, description);*/
+        }
+    }
+}
+        
