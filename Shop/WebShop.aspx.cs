8c6c69ab4f6a8cc69f2f9006126c25034a8505e2
toxa258 <anton.lipin00@mail.ru>
2020-03-05 14:37:02 +0300

    Продажа

--- Shop/WebShop.aspx.cs
+++ Shop/WebShop.aspx.cs
@@ -8,13 +8,12 @@ namespace Shop
 {
     public partial class WebForm1 : System.Web.UI.Page
     {
-        string connectionString;
-
+        string connectionString = ConfigurationManager.ConnectionStrings["connectionString"].ConnectionString;
         protected void Page_Load(object sender, EventArgs e)
         {
-            connectionString = ConfigurationManager.ConnectionStrings["connectionString"].ConnectionString;
+
         }
-       
+
         protected void TextBox1_TextChanged(object sender, EventArgs e)
         {
 
@@ -39,17 +38,19 @@ namespace Shop
 
         protected void Delete_Click(object sender, EventArgs e)
         {
-            var service = new BalanceProductService(connectionString);
-            if (GridView1.SelectedDataKey == null)
-            {
-                return;
-            }
             string productId = GridView1.SelectedDataKey["Id"].ToString();
-               string tableName = "Products";
+            string tableName = "Products";
+            var service = new BalanceProductService(connectionString);
             service.DeleteFromTable(tableName, productId);
             GridView1.DataBind();
         }
+
         protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void SqlDataSource1_Selecting(object sender, System.Web.UI.WebControls.SqlDataSourceSelectingEventArgs e)
         {
 
         }
