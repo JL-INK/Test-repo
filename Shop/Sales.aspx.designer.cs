6758c60278cbc1f88fd5be575c2d92d695c3619a
toxa258 <anton.lipin00@mail.ru>
2020-03-05 14:59:52 +0300

    https://github.com/JL-INK/Test-repo.git

--- Shop/Sales.aspx.designer.cs
+++ Shop/Sales.aspx.designer.cs
@@ -10,7 +10,7 @@
 namespace Shop {
     
     
-    public partial class Warehouse {
+    public partial class Sales {
         
         /// <summary>
         /// form1 элемент управления.
@@ -22,22 +22,31 @@ namespace Shop {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
-        /// DropDownList1 элемент управления.
+        /// GridView1 элемент управления.
         /// </summary>
         /// <remarks>
         /// Автоматически создаваемое поле.
         /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.DropDownList DropDownList1;
+        protected global::System.Web.UI.WebControls.GridView GridView1;
         
         /// <summary>
-        /// Date элемент управления.
+        /// Id элемент управления.
         /// </summary>
         /// <remarks>
         /// Автоматически создаваемое поле.
         /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox Date;
+        protected global::System.Web.UI.WebControls.DropDownList Id;
+        
+        /// <summary>
+        /// SqlDataSource3 элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSource3;
         
         /// <summary>
         /// Quantity элемент управления.
@@ -49,6 +58,15 @@ namespace Shop {
         protected global::System.Web.UI.WebControls.TextBox Quantity;
         
         /// <summary>
+        /// Date элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox Date;
+        
+        /// <summary>
         /// Add элемент управления.
         /// </summary>
         /// <remarks>
@@ -58,15 +76,6 @@ namespace Shop {
         protected global::System.Web.UI.WebControls.Button Add;
         
         /// <summary>
-        /// GridView1 элемент управления.
-        /// </summary>
-        /// <remarks>
-        /// Автоматически создаваемое поле.
-        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.GridView GridView1;
-        
-        /// <summary>
         /// SqlDataSource1 элемент управления.
         /// </summary>
         /// <remarks>
@@ -74,6 +83,24 @@ namespace Shop {
         /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
         /// </remarks>
         protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSource1;
+        
+        /// <summary>
+        /// GridView2 элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView GridView2;
+        
+        /// <summary>
+        /// SqlDataSource2 элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSource2;
         
         /// <summary>
         /// Delete элемент управления.
--- Warehouse/Models/SalesProduct.cs
+++ Warehouse/Models/SalesProduct.cs
@@ -6,40 +6,27 @@ using System.Threading.Tasks;
 
 namespace Warehouse.Models
 {
-    /// <summary>
-    /// Остаток товара на складе
-    /// </summary>
-    public class BalanceProduct
+    class SalesProduct
     {
         /// <summary>
         /// ИД
         /// </summary>
         public int Id { get; set; }
-
-        /// <summary>
-        /// ИД продукта
-        /// </summary>
-        public int ProductId { get; set; }
-
         /// <summary>
         /// Название
         /// </summary>
         public string Name { get; set; }
-
-        /// <summary>
-        /// Описание
-        /// </summary>
-        public string Description { get; set; }
-
         /// <summary>
         /// Количество
         /// </summary>
         public int Quantity { get; set; }
-
         /// <summary>
         /// Дата
         /// </summary>
-        public DateTime Date { get; set; }
+        public int Date { get; set; }
+
+
+
 
     }
 }
