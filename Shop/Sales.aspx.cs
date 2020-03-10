91bbf8d527ae804ecf2c116cfc247fe023ab9015
toxa258 <anton.lipin00@mail.ru>
2020-03-10 09:29:37 +0300

    https://github.com/JL-INK/Test-repo.git

---
+++ Shop/Sales.aspx.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Configuration;
+using System.Web.UI.WebControls;
+using Warehouse.Models;
+using Warehouse.Service;
+
+namespace Shop
+{
+    public partial class Sales : System.Web.UI.Page
+    {
+        string connectionString;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            connectionString = ConfigurationManager.ConnectionStrings["connectionString"].ConnectionString;
+
+        }
+
+        protected void SqlDataSource1_Selecting(object sender, SqlDataSourceSelectingEventArgs e)
+        {
+
+        }
+        protected void Add_Click(object sender, EventArgs e)
+        {
+            var service = new BalanceProductService(connectionString);
+            var product = new BalanceProduct();
+            string i = Id.Text.ToString();
+            int id = Convert.ToInt32(i);
+            product.ProductId = id;
+            string d = Date.Text.ToString();
+            DateTime date = Convert.ToDateTime(d);
+            product.Date = date;
+            string q = Quantity.Text.ToString();
+            int quantity = Convert.ToInt32(q);
+            product.Quantity = quantity;
+            service.AddSales(id, quantity, date);
+            GridView1.DataBind();
+            GridView2.DataBind();
+        }
+
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void GridView2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        protected void Id_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        protected void Quantity_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        protected void Date_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        protected void Id_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Delete_Click(object sender, EventArgs e)
+        {
+            var service = new BalanceProductService(connectionString);
+            if (GridView2.SelectedDataKey == null)
+            {
+                return;
+            }
+            string id = GridView2.SelectedDataKey["Id"].ToString();
+            string tableName = "Sales";
+            service.DeleteFromTable(tableName, id);
+            GridView1.DataBind();
+            GridView2.DataBind();
+        }
+    }
+}
---
+++ Shop/Sales.aspx.designer.cs
@@ -0,0 +1,114 @@
+﻿//------------------------------------------------------------------------------
+// <автоматически создаваемое>
+//     Этот код создан программой.
+//
+//     Изменения в этом файле могут привести к неправильной работе и будут потеряны в случае
+//     повторной генерации кода. 
+// </автоматически создаваемое>
+//------------------------------------------------------------------------------
+
+namespace Shop {
+    
+    
+    public partial class Sales {
+        
+        /// <summary>
+        /// form1 элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        
+        /// <summary>
+        /// GridView1 элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView GridView1;
+        
+        /// <summary>
+        /// Id элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList Id;
+        
+        /// <summary>
+        /// SqlDataSource3 элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSource3;
+        
+        /// <summary>
+        /// Quantity элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox Quantity;
+        
+        /// <summary>
+        /// Date элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox Date;
+        
+        /// <summary>
+        /// Add элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Add;
+        
+        /// <summary>
+        /// SqlDataSource1 элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSource1;
+        
+        /// <summary>
+        /// GridView2 элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView GridView2;
+        
+        /// <summary>
+        /// SqlDataSource2 элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSource2;
+        
+        /// <summary>
+        /// Delete элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Delete;
+    }
+}
